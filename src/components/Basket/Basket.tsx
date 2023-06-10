51ba126caf9b3e7a8154051a2274dea98525f8dd
Koray Aydemir <korayaydemir276@gmail.com>
2023-06-10 14:19:57 +0300

    feat: basket follows the mouse

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -1,9 +1,32 @@
+import { useCallback, useEffect, useState } from "react";
 import { ReactComponent as BasketSvg } from "../../assets/apple_basket-cropped.svg";
 export const Basket = () => {
+    const [pos, setPos] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = useCallback(
+        (e: Event) => {
+            const { clientX } = e as MouseEvent;
+
+            setPos({ x: clientX, y: pos.y });
+        },
+        [pos.y]
+    );
+
+    useEffect(() => {
+        document.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, [handleMouseMove]);
     return (
         <div
             id="basket"
-            className="absolute w-[200px] bottom-[150px] right-[50px]"
+            className="absolute w-[160px] bottom-[100px]"
+            style={{ transform: `translateX(-50%)`, left: pos.x }}
+            onMouseMove={
+                handleMouseMove as unknown as React.MouseEventHandler<HTMLDivElement>
+            }
         >
             <BasketSvg />
         </div>

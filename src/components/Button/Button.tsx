e1cda67cd6a8e8fa663749ca552f71421a440945
Koray Aydemir <korayaydemir276@gmail.com>
2023-06-10 20:13:09 +0300

    feat: dirty solution to disable button until apples have fallen

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 export const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
     return (
         <button
-            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
             {...rest}
         >
             {children}
--- src/components/ShakeTreeButton/ShakeTreeButton.tsx
+++ src/components/ShakeTreeButton/ShakeTreeButton.tsx
@@ -1,31 +1,41 @@
 import { Button } from "../Button/Button";
 import { useDispatch } from "react-redux";
 import { setShouldShake, setIsShakeDone } from "../../redux/slices/shakeSlice";
+import { useState } from "react";
 export const ShakeTreeButton = () => {
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
     const dispatch = useDispatch();
 
     const clickHandler = () => {
         dispatch(setIsShakeDone(false));
         dispatch(setShouldShake(true));
+        setIsButtonDisabled(true);
 
         const shakeDuration = setTimeout(() => {
             dispatch(setShouldShake(false));
         }, 3000);
 
         const yo = setTimeout(() => {
-            // if it is set at the same time as shouldShake, it will not work
-            // to observe: put this dispatch inside shakeDuration
+            // If it is set at the same time as shouldShake, animation
+            // will not work as expected, must be a css thing.
+            // To observe: put this dispatch inside shakeDuration
             dispatch(setIsShakeDone(true));
         }, 3110);
+
+        const enableButton = setTimeout(() => {
+            setIsButtonDisabled(false);
+        }, 9000);
 
         return () => {
             clearTimeout(shakeDuration);
             clearTimeout(yo);
+            clearTimeout(enableButton);
         };
     };
     return (
-        <div className="mt-10 md:absolute md:bottom-[200px]">
-            <Button onClick={clickHandler}>
+        <div className={`mt-10 md:absolute md:bottom-[200px] `}>
+            <Button onClick={clickHandler} disabled={isButtonDisabled}>
                 <span className="text-2xl">Shake it! </span>
             </Button>
         </div>

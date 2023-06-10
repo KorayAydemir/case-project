3ffd9f1bb629c12c2ae989c010c7b4f97bb55c33
Koray Aydemir <korayaydemir276@gmail.com>
2023-06-10 18:01:38 +0300

    feat: can move basket on touch screens

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -4,15 +4,29 @@ export const Basket = () => {
     const [pos, setPos] = useState({ x: 0 });
 
     const handleMouseMove = useCallback((e: Event) => {
-        const { clientX } = e as MouseEvent;
-        setPos({ x: clientX });
+        e.preventDefault();
+        if ("touches" in e) {
+            const touchEvent = e as TouchEvent;
+            if (!touchEvent.touches[0]) {
+                return;
+            }
+            const { clientX } = touchEvent.touches[0];
+            setPos({ x: clientX });
+        } else {
+            const { clientX } = e as MouseEvent;
+            setPos({ x: clientX });
+        }
     }, []);
 
     useEffect(() => {
         document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("touchmove", handleMouseMove, {
+            passive: false,
+        });
 
         return () => {
             document.removeEventListener("mousemove", handleMouseMove);
+            document.addEventListener("touchmove", handleMouseMove);
         };
     }, [handleMouseMove]);
     return (

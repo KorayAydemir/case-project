8598ea00b2bba9f2879f95d191b39c78fdc698e7
Koray Aydemir <korayaydemir276@gmail.com>
2023-06-10 18:24:01 +0300

    feat: make tailwind breakpoints desktop-first

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
+        screens: {
+            // Make responsiveness desktop first
+            "2xl": { max: "1535px" },
+            xl: { max: "1279px" },
+            lg: { max: "1023px" },
+            md: { max: "767px" },
+            sm: { max: "639px" },
+        },
     },
     plugins: [],
 };

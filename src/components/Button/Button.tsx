1ea35e349fb43a5f1ed484f24e52e9ceab4707bd
Koray Aydemir <korayaydemir276@gmail.com>
2023-06-11 15:46:30 +0300

    fix: make button text not-selectable

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ export const Button: React.FC<ButtonProps> = ({
 }) => {
     return (
         <button
-            className={`${className} bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed`}
+            className={`${className} bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed select-none`}
             {...rest}
         >
             {children}
